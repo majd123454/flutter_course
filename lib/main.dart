d7a4697b5fa820b6362d45853813f2bafb0e81bf
Bshr <majdsh7512@gmail.com>
2026-03-08 10:20:58 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              color: Colors.amber,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
+    );
+    
+  }
+}
